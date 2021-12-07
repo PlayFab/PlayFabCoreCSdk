e1511c0c635c2989eff90f04da415638ba17b204
Jason Sandlin <jasonsa@microsoft.com>
2021-12-07 14:00:54 -0800

    Adding ADO build pipeline support

---
+++ code/source/build_version.h
@@ -0,0 +1,5 @@
+// Copyright (c) Microsoft Corporation
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+#pragma once
+ 
+#define PF_SDK_API_VERSION_STRING "2022.01.0.0" 
